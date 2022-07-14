af494df1d2d6d2d7cbf08548b7c53c21334862fd
Werner Almesberger <werner@linzhi.io>
2022-07-14 16:30:30 -0300

    alloc.h (stralloc_append): add string to string, modifying the first

--- alloc.h
+++ alloc.h
@@ -1,7 +1,7 @@
 /*
  * alloc.h - Common allocation functions
  *
- * Copyright (C) 2021 Linzhi Ltd.
+ * Copyright (C) 2021, 2022 Linzhi Ltd.
  * Copyright 2016 Werner Almesberger
  *
  * This work is licensed under the terms of the MIT License.
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 
 
 #define alloc_size(s)					\
@@ -53,5 +54,17 @@
        memcpy(strnalloc_tmp, s, n);			\
        strnalloc_tmp[n] = 0;				\
        strnalloc_tmp; })
+
+
+static inline char *stralloc_append(char *a, const char *b)
+{
+	size_t len_a = a ? strlen(a) : 0;
+	size_t len_b = strlen(b);
+	size_t len = len_a + len_b;
+
+	a = realloc_size(a, len + 1);
+	memcpy(a + len_a, b, len_b + 1);
+	return a;
+}
 
 #endif /* !LINZHI_LIBCOMMON_ALLOC_H */
